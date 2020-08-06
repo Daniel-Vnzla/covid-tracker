11b95ee7f6da75f2c70dbe48380b6e16bda597c3
unknown <rune.escape888@gmail.com>
2020-08-06 09:12:22 -0400

    Updates

---
+++ precache-manifest.02e75471e516de77ab10b7b6056c6507.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bee287ac46eb321b4e60bca14db2038",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "7a4d4d35aaafe5dcaecc",
+    "url": "/covid-tracker/static/css/main.0e971bd9.chunk.css"
+  },
+  {
+    "revision": "f9c8818e3694fab68acc",
+    "url": "/covid-tracker/static/js/2.4c0b1db9.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.4c0b1db9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a4d4d35aaafe5dcaecc",
+    "url": "/covid-tracker/static/js/main.be8782f5.chunk.js"
+  },
+  {
+    "revision": "2b88debb37801485fe3c",
+    "url": "/covid-tracker/static/js/runtime-main.cff7310c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/covid-19.d7265326.png"
+  }
+]);
